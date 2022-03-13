651ed3e64fbfddfa30ffc7c9e81cdf6637b10f8d
ejjang2030 <ejjang2030@gmail.com>
2022-03-13 16:55:32 +0900

    state managment with redux in react application

---
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,22 @@
+import { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Counter from "../components/Counter";
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+    return (
+        <Counter 
+            number={number}
+            onIncreate={onIncrease}
+            onDecrease={onDecrease}
+        />
+    );
+};
+
+export default CounterContainer;
